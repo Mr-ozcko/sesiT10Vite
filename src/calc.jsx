3eb149de37996ab2e4d019ff3309015e375a2e2c
mr-ozcko <luizc.mroczko.com>
2024-03-28 11:26:02 -0300

    adicionando o zap

--- src/calc.jsx
+++ src/calc.jsx
@@ -1,12 +1,64 @@
 import style from "./calc.module.css"
 import Menu from "./components/menu"
+import { useState, useEffect } from "react"
 
 export const Calc = () =>{
+    const [numero1, setNumero1] = useState()
+    const [numero2, setNumero2] = useState()
+    const [respSoma, setRespSoma] = useState()
+    const [respSub, setRespSub] = useState()
+    const [respMulti, setRespMulti] = useState()
+    const [respDiv, setRespDiv] = useState()
+
+    // useEffect(()=> {
+
+    //     if(numero1 !== undefined && numero2 !== undefined){
+    //         setRespSoma(parseFloat(numero1) + parseFloat(numero2))
+    //         setRespSub(parseFloat(numero1) - parseFloat(numero2))
+    //         setRespMulti(parseFloat(numero1) * parseFloat(numero2))
+    //         setRespDiv(parseFloat(numero1) / parseFloat(numero2)) 
+    //     }
+
+    // }, [numero1, numero2])
+
+    const handleSoma = () => setRespSoma(parseFloat(numero1) + parseFloat(numero2))
+    const handleSub = (a,b) => setRespSub(parseFloat(a) - parseFloat(b))
+
+    const msgErro = "Digite um numreo valido"
+
     return(
         <>
         <Menu/>
         <div className={style.sectionCalc}>
             <h1>Calculos</h1>
+            <div>
+                <label htmlFor="n1"> numero 1</label>
+                <input type="text" id="n1" value={numero1} onChange={(e) => setNumero1(e.target.value)}/>
+            </div>
+            <div>
+                <label htmlFor="n2"> numero 2</label>
+                <input type="text" id="n2" value={numero2} onChange={(e) => setNumero2(e.target.value)}/>
+            </div>
+            <div>
+                <div>
+                    <h1>Soma</h1>
+                    {!isNaN(respSoma) ? respSoma : msgErro}
+                    <button onClick={handleSoma}>Somar</button>
+                </div>
+                <div>
+                    <h1>Sub</h1>
+                    {!isNaN(respSub) ? respSub : msgErro}
+                    <button onClick={() => handleSub(numero1, numero2)}> Subtrair</button>
+                </div>
+                <div>
+                    <h1>Mult</h1>
+                    {!isNaN(respMulti) ? respMulti : msgErro}
+                </div>
+                <div>
+                    <h1>Div</h1>
+                    {!isNaN(respSoma) ? respDiv : msgErro}
+                </div>
+            </div>
         </div>
         </>
     )
--- src/contact.jsx
+++ src/contact.jsx
@@ -5,17 +5,60 @@ import Menu from "./components/menu"
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
 
 import style from "./contact.module.css"
+import { useState } from 'react';
 
 export const Contact = () => {
     const position = [-25.4249717, -49.2748809,17]
+    const deafPhoneNum = "5541999999999"
+
+    const [formData, setFormData] = useState({
+        name: "",
+        email: "",
+        message: ""
+    })
+
+    const handleChange = (e) => {
+        const {name, value} = e.target
+        setFormData({...formData, [name]: value})
+    }
+
+    const handleZap = () => {
+        const {name, email, message} = formData
+
+        const urlZap = `https://api.whatsapp.com/send?phone=${deafPhoneNum}&text=
+        Nome:%20${name}%0D%0A
+        Email:%20${email}%0D%0A
+        Message:%20${message}%0D%0A`
+
+        window.open(urlZap, '_blank')
+
+    }
+
     return(
         <>
         <Menu/>
         <div className= {style.sectionContact}>
             <h1>contato</h1>
             <div>
-                <h2>mapa</h2>
                 <div>
+                    <h2>contatos</h2>
+                    <div>
+                    <label htmlFor="name">nome</label>
+                    <input type="text" id='name' name='name' value={formData.name} onChange={handleChange} required/>
+                    </div>
+                    <div>
+                    <label htmlFor="email">email</label>
+                    <input type="text" id='email' name='email' value={formData.email} onChange={handleChange} required/>  
+                    </div>
+                    <div>
+                    <label htmlFor="message">mensagem</label>
+                    <textarea name="message" id="message" value={formData.message} onChange={handleChange} required cols="30" rows="10"></textarea>                        
+                    </div>
+                    <button onClick={handleZap}>enviar msg</button>
+                </div>
+
+                <div>
+                <h2>mapa</h2>
                 <MapContainer center={position} zoom={13} scrollWheelZoom={false} style={{width: "100%", height: "700px"}}>
                     <TileLayer
                         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
